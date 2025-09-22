8734283d4051de50d7f633f9c6233e78f2bed261
Pravin Gaikwad <gaikwadpravin667@gmail.com>
2025-09-22 18:15:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 if [ "$RENDER" = "true" ]; then
   uvicorn fresh_glassdoor_scraper:app --host 0.0.0.0 --port $PORT
 else
-  uvicorn fresh_glassdoor_scraper:app --reload
+  uvicorn fresh_glassdoor_scraper:app --host 127.0.0.1 --port 8000 --reload
 fi
